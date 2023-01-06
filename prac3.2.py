e6f31cb5b3ae065638d6751c251c8c8b15c57ed2
KavanGandhi <kavangandhi1@gmail.com>
2023-01-06 23:10:06 +0530

    Added

--- prac3.2.py
+++ prac3.2.py
@@ -2,17 +2,23 @@ D={
     "Dept":"CE",
     "College":"DEPSTAR",
     "ID":"21DCE023"}
+#Obtaining key using keys function
 print("Keys in dictionary are",D.keys());
+#Obtaining values using values function
 print("Values  in dictionary are",D.values());
+#updating a dictionary
 D.update({"ID":"21DCE000"})
 print("Updated dictionary is",D)
+#removing a key from dictionary
 D.pop("ID")
 print(D)
+#removing last element in dictionary
 D.popitem()
 print(D)
-
+#obtatining values
 x= D.get("Dept")
 print("Department value from dictionary is",x)
+#clearing whole dictionary
 D.clear()
 print(D)
 
@@ -27,7 +33,9 @@ D2={"Dept2":"IT",
 D3={"Dept3":"CE",
     "College3":"ddkeXWXX",
     "ID3":"21DCE02000"}
+#merging all dictionaries
 D4={**D1,**D2,**D3}
 print(D4)
+print("21DCE023 KAVAN GANDHI")
 
 
