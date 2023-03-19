a6ca30035f67ae7a8b26f5166889e41bde921140
KavanGandhi <kavangandhi1@gmail.com>
2023-03-19 23:01:05 +0530

    leetcode merge linked list

--- duplicate_LL.java
+++ duplicate_LL.java
@@ -1,12 +1,7 @@
  class duplicate_LL {
     private Node head;
     private Node tail;
-    private int size;
-
-    public duplicate_LL(){
-        this.size=0;
-    }
-
+    
     public void insertFirst(int val){
         Node node = new Node(val);
         node.next=head;
@@ -35,7 +30,6 @@
         while(node.next!=null){
         if(node.value == node.next.value){
             node.next = node.next.next;
-            size--;
         }
         else{
             node = node.next;
@@ -44,6 +38,36 @@
         node.next = null;
     }
 }
+
+    public static duplicate_LL merge(duplicate_LL first, duplicate_LL second)
+    {
+        Node f = first.head;
+        Node s = second.head;
+
+        duplicate_LL ans = new duplicate_LL();
+
+        while(f.next!=null && s.next!=null){
+            if(f.value>s.value){
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+            else{
+                ans.insertLast(f.value);
+                f = f.next;
+            }
+        }
+        while(f!=null){
+            ans.insertLast(f.value);
+            f=f.next;
+        }
+        while(s!=null){
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+
+        return ans;
+
+    }
 
     public void display(){
         Node temp=head;
@@ -69,23 +93,23 @@
         //     this.next=next;
         // }
     }
-}
 
-class leet_duplicate{
     public static void main(String[] args) {
-        duplicate_LL l = new duplicate_LL();
-        l.insertLast(1);
-        l.insertLast(1);
-        l.insertLast(2);
-        l.insertLast(3);
-        l.insertLast(3);
-        l.insertLast(4);
-       
-        l.display();
-        l.duplicate();
-        l.display();
-        
+        duplicate_LL first = new duplicate_LL();
+        duplicate_LL second = new duplicate_LL();
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        duplicate_LL ans = merge(first,second);
+        ans.display();
     }
 }
+
+
 
 
