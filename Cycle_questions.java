987a836be71b8bd2f24f491a85b5661ffb8e75c5
KavanGandhi <kavangandhi1@gmail.com>
2023-03-24 15:17:26 +0530

    codechef problem added

--- Cycle_questions.java
+++ Cycle_questions.java
@@ -26,8 +26,9 @@ public class Cycle_questions {
                     slow = slow.next; 
                     size++;
                 }while(fast != slow);
+                return size;
+
             }
-            return size;
         }
         return 0;
     }
